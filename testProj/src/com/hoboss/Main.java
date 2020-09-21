a4a941f8c35fa0ebb4e67c3e3b77c227a56a3b7f
hobossa <hoboss@gmail.com>
2020-09-22 01:09:44 +0800

    Update Main.java

--- testProj/src/com/hoboss/Main.java
+++ testProj/src/com/hoboss/Main.java
@@ -60,8 +60,10 @@ public class Main {
 
     public static void main(String[] args) {
         int[][] matrix = getMatrix(100);
+        System.out.println(" | 2 3 7 total");
+        System.out.println("-+------------");
         for (int i = 0; i < matrix.length; ++i) {
-            System.out.print("" + i + ":");
+            System.out.print("" + i + "|");
             for (int j = 0; j < matrix[i].length; ++j) {
                 System.out.print(" " + matrix[i][j]);
             }
